d6b3bb93310a0fa32c778f3d0f09204c863e55e7
madeas <an.sharapov90@gmail.com>
2018-10-05 10:44:14 +0300

    preparation to npm

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+let gulp        = require('gulp'),
+    sass        = require('gulp-ruby-sass'),
+    sourcemaps  = require('gulp-sourcemaps'),
+    rename      = require("gulp-rename"),
+    csso        = require('gulp-csso');
+
+// sass
+gulp.task('sass', () =>
+    sass('scss/box-shadows.scss', {sourcemap: true}).on('error', sass.logError)
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./'))
+);
+
+// csso
+gulp.task('csso', function () {
+    return gulp.src('./**/box-shadows.css')
+        .pipe(rename('./box-shadows.min.css'))
+        .pipe(sourcemaps.init())
+        .pipe(csso())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('watch', ['sass','js', 'css'], function () {
+    gulp.watch('scss/*.scss', ['sass'])
+});
+
+gulp.task('default', ['watch']);
